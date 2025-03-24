2c761fa4062dac0871c5449af6b8e1fdbc026f28
muhammad uzair <abuzada735@gmail.com>
2025-03-24 09:38:09 +0500

    assignment done
    done

---
+++ index.js
@@ -0,0 +1,21 @@
+function converter(usd) {
+  let pkr = usd * 280;
+  document.write(pkr);
+}
+converter(20);
+
+function converter2(pkr) {
+  let usd = pkr / 280;
+  document.write(usd);
+}
+converter2(10000);
+
+//  function converter3(pkr,usd){
+//     let pkr=usd*280;
+//     document.write(pkr);
+
+//     let usd=pkr/280;
+//     document.write(usd);
+
+//  }
+//  converter3(10,10000);
